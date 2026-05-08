0f9c0181e8191eebae4ed29eec3ffece5bb3dab1
Samudika1210 <samudikasw10@gmail.com>
2026-05-08 18:53:34 +0530

    add vehicle parent class

---
+++ LabSheet_4/src/Lab_05/vehicle.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab_05;
+
+/**
+ *
+ * @author ASUS
+ */
+public class vehicle {
+    private String vehicleNumber;
+    protected String brand;
+
+    public vehicle(String vehicleNumber, String brand) {
+        this.vehicleNumber = vehicleNumber;
+        this.brand = brand;
+    }
+
+    public void displayVehicleInfo() {
+        System.out.println("Vehicle Number: " + vehicleNumber + " | Brand: " + brand);
+    }
+
+    public double calculateRentalCost() {
+        System.out.println("General vehicle rental cost calculation");
+        return 0.0;
+    }
+    
+}
